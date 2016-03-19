0d989afb557cd147b72c2290e3893d717bcf3e21
Szymon <furmanszymon@gmail.com>
2016-03-19 22:11:19 +0100

    Add register.html, loginCtrl, registerCtrl,

---
+++ app/controllers/registerCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Szymon on 19.03.2016.
+ */
+(function (mainApp) {
+    mainApp.controller('registerCtrl', ['$scope', function ($scope) {
+        "use strict";
+
+
+    }]);
+}(angular.module('mainApp')));
